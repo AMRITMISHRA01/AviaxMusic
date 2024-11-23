424dc1dcdbd6173a4721705c929d9f29f223f777
Amrit Mishra <bkmishra77amrit@gmail.com>
2024-11-23 21:54:18 +0530

    config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CyberPixelPro/AviaxMusic",
+    "https://github.com/AMRITMISHRA01/AviaxMusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
